6045429f1c64e1d9dfec6148c7ee852a94c6a504
Milana <milana.zaborovska@gmail.com>
2021-06-29 13:09:53 +0100

    added a fizzbuzz if statement

--- Section 10 Exercises/FizzBuzz.js
+++ Section 10 Exercises/FizzBuzz.js
@@ -1,9 +1,19 @@
- var output = [];
- number = 1;
+var output = [];
+number = 1;
 
- function FizzBuzz() {
-     output.push(number);
-     number ++;
-     return output;
- }
-
+function FizzBuzz() {
+    if (number % 3 === 0 && number % 5 === 0){
+        output.push("FizzBuzz");
+    } 
+    else if (number % 3 === 0){
+        output.push("Fizz");
+    } 
+    else if (number % 5 === 0){
+        output.push("Buzz");
+    } 
+    else{
+        output.push(number);
+    }
+    number ++;
+    return output;
+}
