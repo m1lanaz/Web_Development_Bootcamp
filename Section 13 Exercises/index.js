8f9a3fe60782d6703337a04b1dc7fb1e682696ca
Milana <milana.zaborovska@gmail.com>
2021-07-03 20:15:01 +0100

    changed css images and made them play audio

--- Section 13 Exercises/index.js
+++ Section 13 Exercises/index.js
@@ -1,8 +1,7 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++ ) {
     document.querySelectorAll("button")[i].addEventListener("click", function () {
-        alert("I got clicked!");
-        //What to do when the click is detected
-    });
+
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+});
 }
-
-
