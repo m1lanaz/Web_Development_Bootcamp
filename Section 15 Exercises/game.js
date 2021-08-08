d9b61596074c94407995140e670289b1609e39fe
Milana <milana.zaborovska@gmail.com>
2021-08-08 13:11:36 +0100

    added a way to check that the users entered click match up to the sequence

--- Section 15 Exercises/game.js
+++ Section 15 Exercises/game.js
@@ -1,8 +1,43 @@
 var buttonColours = ["red","blue","green","yellow"];
 
 var gamePattern = [];
-
 var userClickedPattern = [];
+
+var level = 0;
+
+var gameStarted = false;
+
+$(document).keypress(function() {
+    if (!gameStarted) {
+  
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  });
+
+  $(".btn").click(function(event){
+    var userChosenColor = $(this).attr("id");
+    playSound(userChosenColor);
+    animatePress("." + userChosenColor);
+    userClickedPattern.push(userChosenColor);
+
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function nextSequence(){
+    userClickedPattern = []
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomchosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomchosenColour);
+ 
+    $("#" + randomchosenColour).fadeOut(100).fadeIn(100);
+    playSound(randomchosenColour);
+
+    level+=1;
+    $("#level-title").text("Level " + level);
+}
+
 
 function playSound(name){
     new Audio("sounds/" + name + ".mp3").play();
@@ -16,19 +51,28 @@ function animatePress(currentColor){
 
 }
 
-function nextSequence(){
-    var randomNumber = Math.floor(Math.random() * 4);
-    randomchosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomchosenColour);
- 
-    $("#" + randomchosenColour).fadeOut(100).fadeIn(100);
-    playSound(randomchosenColour);
+function checkAnswer(currentLevel){
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else{
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
 }
 
-$(".btn").click(function(event){
-    var userChosenColor = $(this).attr("id");
-    playSound(userChosenColor);
-    animatePress("." + userChosenColor);
-    userClickedPattern.push(userChosenColor);
-});
-
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
